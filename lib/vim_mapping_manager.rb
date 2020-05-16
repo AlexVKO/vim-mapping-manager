7775f334c14bf8f53616a51d78c7f887f02a83fa
AlexVKO <ale@alexvko.com>
2020-05-16 12:12:01 -0400

    Adds prefix functionality

--- lib/vim_mapping_manager.rb
+++ lib/vim_mapping_manager.rb
@@ -1,6 +1,78 @@
 require "vim_mapping_manager/version"
+require 'tempfile'
 
 module VimMappingManager
   class Error < StandardError; end
-  # Your code goes here...
+
+  @global_key_strokes = {}
+
+  def self.prefix(key, name:, desc:, &block)
+    find_key_stroke(key).set_prefix(name: name, desc: desc)
+    find_key_stroke(key).instance_exec(&block)
+  end
+
+  def self.render
+    @global_key_strokes.values.each(&:render)
+    OutputFile.output
+  end
+
+  private
+
+  def self.find_key_stroke(key)
+    @global_key_strokes[key] ||= KeyStroke.new(key)
+  end
+
+  class KeyStroke
+    attr_reader :key, :prefix
+
+    def initialize(key)
+      @key = key
+    end
+
+    # Set prefix informations for this key stroke
+    def set_prefix(name:, desc:)
+      raise("#{key} is already defined as a prefix") if prefix
+
+      @prefix = Prefix.new(name, desc)
+    end
+
+    # Defines a normal command
+    def normal(key, command, desc: nil)
+    end
+
+    def render
+      prefix.render(key)
+    end
+  end
+
+  class Prefix
+    attr_reader :name, :desc
+
+    def initialize(name, desc)
+      @name = name
+      @desc = desc
+    end
+
+    def render(key)
+      OutputFile.write '" -----------------------------------------------------------------------------'
+      OutputFile.write "\" Prefix: #{name}, key: #{key}"
+      OutputFile.write "\" #{desc}"
+      OutputFile.write '" -----------------------------------------------------------------------------'
+      OutputFile.write "  nnoremap  [#{name}] <Nop>"
+      OutputFile.write "  nmap      #{key} [Files]"
+    end
+  end
+end
+
+class OutputFile
+  @tempfile = ::Tempfile.new()
+
+  def self.write(content)
+    @tempfile << content + "\n"
+  end
+
+  def self.output
+    @tempfile.rewind
+    @tempfile.read
+  end
 end
--- spec/vim_mapping_manager_spec.rb
+++ spec/vim_mapping_manager_spec.rb
@@ -1,9 +1,43 @@
 RSpec.describe VimMappingManager do
   it "has a version number" do
-    expect(VimMappingManager::VERSION).not_to be nil
+    expect(VimMappingManager::VERSION).not_to be "0.1.0"
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe 'Simple mapping' do
+    it 'renders prefixes' do
+      described_class.prefix(';', name: 'FuzzyFinder', desc: 'Search everything') do
+        # normal('du', ":saveas #{current_file_path}", desc: "Duplicate current file")
+      end
+
+      expected = <<-EXPECTED
+       " -----------------------------------------------------------------------------
+       " Prefix: FuzzyFinder, key: ;
+       " Search everything
+       " -----------------------------------------------------------------------------
+       nnoremap  [FuzzyFinder] <Nop>
+       nmap      ; [Files]
+      EXPECTED
+
+      renders_properly(described_class.render, expected)
+    end
+
+    it 'renders normal mode mappings'  do
+      described_class.normal('du', ":saveas #{current_file_path}", desc: "Duplicate current file")
+
+      expected = <<-EXPECTED
+      EXPECTED
+
+      renders_properly(described_class.render, expected)
+    end
+
+    def renders_properly(actual, expected)
+      actual = actual.gsub(/^ */, '')
+      expected = expected.gsub(/^ */, '')
+      expect(actual).to include(expected)
+    end
   end
 end
+
+# global.key_stroke('w')
+# KeyStroke<key='w', normal_command: nil, edit_command: nil>
+# global.key_stroke('w').key_stroke('du').normal_command(':saveas #{current_file_path}#{open_command_line_window}')
